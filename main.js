fa2e7e7d0c54b6c260770caebec95eca6bbc6a0b
Austin Erlandson <austin@erlandson.com>
2015-02-02 11:10:01 -0600

    added an IIFE

--- main.js
+++ main.js
@@ -1,64 +1,66 @@
-var $input,
+(function(){
+  'use strict';
+  var $input,
     $ul,
     url   = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
 
-$(function (){
-  $input = $('input'),
-  $ul    = $('ul');
+  $(function (){
+    $input = $('input'),
+    $ul    = $('ul');
 
-  $input.change(getUpdateAndSplit);
-  getUpdateAndSplit();
-});
-
-
-function getUpdateAndSplit(){
-  var count = $input.val();
-  
-  $ul.empty();
-  $.get(url, function(res){
-    var chunkedStudents = chunkData(res['c8-students'], count);
-    $ul.append(createList(chunkedStudents));
+    $input.change(getUpdateAndSplit);
+    getUpdateAndSplit();
   });
-};
 
-function chunkData(data, count){
-  return _(data)
-    .map(function(value){
-      return value.firstName + ' ' + value.lastName[0] + '.';
-    })
-    .shuffle()
-    .chunk(count)
-    .value();
-}
 
-function createList(array) {
-  var groupList = [];
-  _.forEach(array, function(team){
-    var $ol = $('<ol></ol>')
+  function getUpdateAndSplit(){
+    var count = $input.val();
 
-    _.forEach(team, function(teamMember){
-      var $li =  $('<li>' + teamMember + '</li>')
-      $li.text(teamMember) 
-      $ol.append($li);
+    $ul.empty();
+    $.get(url, function(res){
+      var chunkedStudents = chunkData(res['c8-students'], count);
+      $ul.append(createList(chunkedStudents));
+    });
+  };
+
+  function chunkData(data, count){
+    return _(data)
+      .map(function(value){
+        return value.firstName + ' ' + value.lastName[0] + '.';
+      })
+      .shuffle()
+      .chunk(count)
+      .value();
+  }
+
+  function createList(array) {
+    var groupList = [];
+    _.forEach(array, function(team){
+      var $ol = $('<ol></ol>')
+
+      _.forEach(team, function(teamMember){
+        var $li =  $('<li>' + teamMember + '</li>')
+        $li.text(teamMember)
+        $ol.append($li);
+      });
+
+      groupList.push($ol);
+
     });
 
-    groupList.push($ol);
+    return groupList;
 
-  });
+  }
 
-  return groupList;
-
-}
-
-//function getJSON(url, cb) {
-//  var xhr = new XMLHttpRequest();
-//  xhr.open('GET', url);
-//
-//  xhr.onload = function () {
-//    if (this.status >= 200 && this.status < 400) {
-//      cb(JSON.parse(this.response));
-//    }
-//  };
-//
-//  xhr.send();
-//}
+  //function getJSON(url, cb) {
+  //  var xhr = new XMLHttpRequest();
+  //  xhr.open('GET', url);
+  //
+  //  xhr.onload = function () {
+  //    if (this.status >= 200 && this.status < 400) {
+  //      cb(JSON.parse(this.response));
+  //    }
+  //  };
+  //
+  //  xhr.send();
+})();
